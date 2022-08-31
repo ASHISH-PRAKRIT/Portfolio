9e9f30274d11f8c7ae44db9cb78226a5cb136659
ASHISH-PRAKRIT <99477779+ASHISH-PRAKRIT@users.noreply.github.com>
2022-08-31 22:48:06 +0530

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,10 +2,10 @@ const Contact = _=>{
     return (
        <section>
          <h2>Contact Me</h2>
-         <a herf="" target="_blank">Linkedin</a>
-         <a herf="" target="_blank">Facebook</a>
-         <a herf="" target="_blank">Instagram</a>
-         <a herf="" target="_blank">Github</a>
+         <a herf="https://github.com/ASHISH-PRAKRIT" target="_blank">Linkedin</a>
+         <a herf="https://github.com/ASHISH-PRAKRIT" target="_blank">Facebook</a>
+         <a herf="https://github.com/ASHISH-PRAKRIT" target="_blank">Instagram</a>
+         <a herf="https://github.com/ASHISH-PRAKRIT" target="_blank">Github</a>
        </section>
     )
 }
